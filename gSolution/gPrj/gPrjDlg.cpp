fe2e6c995d18bc911ff0aae0b170cc96fbd3ad95
naraeworld <jnrkty1@gmail.com>
2023-10-21 01:58:24 +0900

    듀얼이미지+포인트
    듀얼이미지를 만들고 포인트 갯수 세기 및 좌표 찍기

--- gSolution/gPrj/gPrjDlg.cpp
+++ gSolution/gPrj/gPrjDlg.cpp
@@ -6,6 +6,7 @@
 #include "gPrj.h"
 #include "gPrjDlg.h"
 #include "afxdialogex.h"
+#include <iostream>
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -14,6 +15,7 @@
 #pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
 
 // 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+using namespace std;
 
 class CAboutDlg : public CDialogEx
 {
@@ -65,8 +67,8 @@ BEGIN_MESSAGE_MAP(CgPrjDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
-	ON_BN_CLICKED(IDC_BTN_DLG, &CgPrjDlg::OnBnClickedBtnDlg)
 	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDC_BTN_TEST, &CgPrjDlg::OnBnClickedBtnTest)
 END_MESSAGE_MAP()
 
 
@@ -102,9 +104,16 @@ BOOL CgPrjDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	MoveWindow(0, 0, 1280, 800);
 	m_pDlgImage = new CDlgImage;
 	m_pDlgImage->Create(IDD_DLGIMAGE, this);
 	m_pDlgImage->ShowWindow(SW_SHOW);
+	m_pDlgImage->MoveWindow(0, 0, 640, 480);
+	
+	m_pDlgImageResult = new CDlgImage;
+	m_pDlgImageResult->Create(IDD_DLGIMAGE, this);
+	m_pDlgImageResult->ShowWindow(SW_SHOW);
+	m_pDlgImageResult->MoveWindow(640, 0, 640, 480);
 
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
@@ -160,11 +169,6 @@ HCURSOR CgPrjDlg::OnQueryDragIcon()
 
 
 
-void CgPrjDlg::OnBnClickedBtnDlg()
-{
-	m_pDlgImage->ShowWindow(SW_SHOW);
-	
-}
 
 
 void CgPrjDlg::OnDestroy()
@@ -174,9 +178,38 @@ void CgPrjDlg::OnDestroy()
 	delete m_pDlgImage;
 }
 
-#include <iostream>
+
 void CgPrjDlg::callFunc(int n) {
 
 	//int nData = n;
 	std::cout << n << std::endl;
+}
+
+void CgPrjDlg::OnBnClickedBtnTest()
+{
+	unsigned char* fm = (unsigned char*)m_pDlgImage->m_image.GetBits();
+	int nWidth = m_pDlgImage->m_image.GetWidth();
+	int nHeight = m_pDlgImage->m_image.GetHeight();
+	int nPitch = m_pDlgImage->m_image.GetPitch();
+
+	for (int k = 0; k < 100; k++) {
+		int x = rand() % nWidth;
+		int y = rand() % nHeight;
+		fm[y*nPitch+x] = 0;
+	}
+	
+	int nSum = 0;
+	for (int j = 0; j < nHeight; j++) {
+		for (int i = 0; i < nWidth; i++) {
+			if (fm[j*nPitch + i] == 0) {
+				cout << nSum << "." << i << "," << j << endl;
+				nSum++;
+
+			}
+				
+		}
+	}
+
+	m_pDlgImage->Invalidate();
+
 }
